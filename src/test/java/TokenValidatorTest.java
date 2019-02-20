b30528c2fa7a41346b512245a59f376548632ba7
Brian Campbell <brian.d.campbell@gmail.com>
2019-02-20 12:08:30 -0700

    those are the keys (as a JWK) not signatures

--- src/test/java/TokenValidatorTest.java
+++ src/test/java/TokenValidatorTest.java
@@ -15,35 +15,35 @@ import io.jsonwebtoken.Jwts;
 
 public class TokenValidatorTest
 {
-   // token and signature produced by local keycloak
+   // token and jwk produced by local keycloak
    String token =
          "eyJhbGciOiJFUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJhTUlEWWhuTl9DekE4c2xxSDlfQTJ1cmxfeDNBajVMZjhpQlYyLWx0ZWZvIn0.eyJqdGkiOiJhZTY1NjBhMy0zYTU3LTQ2NWQtYmZkZS1hNDJkYTVjNThiNTYiLCJleHAiOjE1ODIxMDYzMzAsIm5iZiI6MCwiaWF0IjoxNTUwNTcwMzMwLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjIwNDAyL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIxYmYwNmNhOS04Y2ViLTQzNjAtYjhmYy04MjhjNjBhYzI4ZGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJFTVBJQyIsImF1dGhfdGltZSI6MTU1MDU3MDMzMCwic2Vzc2lvbl9zdGF0ZSI6ImI1Zjg5NDJmLThmM2ItNDY5MC04MDA4LTBmNzgwMDhjN2RmMyIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIGV4dGVybmFsLWlkIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJGcmVkIE9wb3NzdW0iLCJleHRlcm5hbElkIjoidGVzdCIsInByZWZlcnJlZF91c2VybmFtZSI6ImZyZWQub3Bvc3N1bS4xMDIxMiIsImdpdmVuX25hbWUiOiJGcmVkIiwiZmFtaWx5X25hbWUiOiJPcG9zc3VtIiwiZW1haWwiOiJmLm9wb3NzdW1AZ214LmRlIn0.MEUCIQCkqrf-FwOitf5vTcCHzOISOAB978ozuhiMLhp3u-3i9gIgSZlRJQqtK6XWJd_KoUA_7O8UrDZ-olZsHCB4ivZUYj0";
-   String signature = "{\"kid\":\"aMIDYhnN_CzA8slqH9_A2url_x3Aj5Lf8iBV2-ltefo\",\"kty\":\"EC\",\"alg\":\"ES256\",\"use\":\"sig\",\"crv\":\"P-256\",\"x\":\"ANraETptwbUJ1-9WvpJNdTCnTFK6uceWYUcPVfH3heDI\",\"y\":\"Jj1NjevefaTd9nZhQ-CRb2LDWwhqhrkCpajujQKSdlY\"}";
+   String jwk = "{\"kid\":\"aMIDYhnN_CzA8slqH9_A2url_x3Aj5Lf8iBV2-ltefo\",\"kty\":\"EC\",\"alg\":\"ES256\",\"use\":\"sig\",\"crv\":\"P-256\",\"x\":\"ANraETptwbUJ1-9WvpJNdTCnTFK6uceWYUcPVfH3heDI\",\"y\":\"Jj1NjevefaTd9nZhQ-CRb2LDWwhqhrkCpajujQKSdlY\"}";
 
-   String invalidSignature = signature.replace("ANraE", "ABCDE");
+   String wrongJwk = jwk.replace("ANraE", "ABCDE");
 
    @Test
    public void testJose4jWithValidSignature()
    {
-      Assert.assertTrue(validateWithJose4jIsOk(token, signature));
+      Assert.assertTrue(validateWithJose4jIsOk(token, jwk));
    }
 
    @Test
    public void testJose4jWithInvalidSignature()
    {
-      Assert.assertFalse(validateWithJose4jIsOk(token, invalidSignature));
+      Assert.assertFalse(validateWithJose4jIsOk(token, wrongJwk));
    }
 
    @Test
    public void testJasonWebTokenWithValidSignature()
    {
-      Assert.assertTrue(validateWithJasonWebTokenIsOk(token, signature));
+      Assert.assertTrue(validateWithJasonWebTokenIsOk(token, jwk));
    }
 
    @Test
    public void testJasonWebTokenWithInvalidSignature()
    {
-      Assert.assertFalse(validateWithJasonWebTokenIsOk(token, invalidSignature));
+      Assert.assertFalse(validateWithJasonWebTokenIsOk(token, wrongJwk));
    }
 
    private static boolean validateWithJasonWebTokenIsOk(String token, String signature)
@@ -88,7 +88,7 @@ public class TokenValidatorTest
       }
       catch (Exception e)
       {
-         throw new RuntimeException("Could not create signature from string", e);
+         throw new RuntimeException("Could not create jwk from string", e);
       }
    }
 }
